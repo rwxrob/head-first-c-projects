ff56341f20b7c6f91bf642e0328f553e205d5a29
rwxrob <rob@rwx.gg>
2021-12-18 21:48:08 -0500

    Add bermuda from chapter 2

--- chapter01/count.c
+++ chapter01/count.c
@@ -3,27 +3,39 @@
 
 int main()
 {
-    char card_name[3];
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-        break;
-    default:
-        // FIXME: replace with strtol
-        val = atoi(card_name);
-    }
-    // fyi: rust does (3..6).contains(&val)
-    if (3 < val && val < 6) 
-        puts("Count has gone up.");
-    else if (val == 10)
-        puts("Count has gone down");
-    return 0;
+        char card_name[3];
+        int count = 0;
+        int done = 0;
+        while (!done) {
+                int val = 0;
+                puts("Enter the card name: ");
+                scanf("%2s", card_name);
+                switch(card_name[0]) {
+                case 'K':
+                case 'Q':
+                case 'J':
+                        val = 10;
+                        break;
+                case 'A':
+                        val = 11;
+                        break;
+                case 'X':
+                        done++;
+                        puts("Goodbye.");
+                        continue;
+                default:
+                        val = atoi(card_name);
+                        if (val < 1 || 10 < val) {
+                                puts("Numbers must be 1-10.");
+                                continue;
+                        }
+                }
+                if (2 < val && val < 7) {
+                        count++;
+                } else if (val == 10) {
+                        count--;
+                }
+                printf("Current count: %i\n", count);
+        }
+        return 0;
 }
---
+++ chapter02/bermuda/bermuda.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void go_south_east(int *lat, int *lon)
+{
+        *lat-=1;
+        *lon+=1;
+        printf("lat: %i (%p) lon: %i (%p)\n", *lat, lat, *lon, lon);
+}
+
+int main()
+{
+      int lat = 0;
+      int lon = 0;
+      go_south_east(&lat,&lon);
+      printf("latitude: %i (%p)\nlongitude: %i (%p)\n",
+                      lat, &lat, lon, &lon);
+      return 0;
+}
+
