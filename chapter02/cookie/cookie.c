00e2a3dd7d25c5898b89c20d1c4e81345f9a9c13
rwxrob <rob@rwx.gg>
2021-12-22 21:17:29 -0500

    Use char *message instead of char message[]

--- chapter02/cookie/cookie.c
+++ chapter02/cookie/cookie.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void cookie(char message[]);
-
 char truth[] = "Cookies make you fat.";
 
-int main()
-{
-        cookie(truth);
-        return 0;
-}
-
-void cookie(char message[])
+//void cookie(char message[]) // the following is EXACTLY the same
+void cookie(char *message)
 {
         printf("%s\n",message);
         // WRONG! printf("Message is %i bytes long?\n",sizeof(message));
         // Following does NOT count the \0 null terminator (man strlen)
         printf("Message is %lu bytes long?\n",strlen(message));
+}
+
+int main()
+{
+        cookie(truth);
+        return 0;
 }
 
